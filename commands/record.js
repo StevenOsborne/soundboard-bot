5afa157032c48ec5add15d23729134070eda7e20
steveno <stevenosborne@hotmail.co.uk>
2019-03-25 21:31:51 +0000

    Use keyphrase mode

--- commands/record.js
+++ commands/record.js
@@ -6,8 +6,7 @@ const modeldir = "/usr/local/share/pocketsphinx/model/en-us/";
 var receiver;
 var config = new ps.Decoder.defaultConfig();
 config.setString("-hmm", modeldir + "en-us");
-config.setString("-dict", modeldir + "cmudict-en-us.dict");
-config.setString("-lm", modeldir + "en-us.lm.bin");
+config.setString("keyphrase", "daffodil");
 
 function generateOutputFile(member) {
     // use IDs instead of username cause some people have stupid emojis in their name
