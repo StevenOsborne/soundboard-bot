53fd7d42c1491da5f055476995950b3f9422e4b3
steveno <stevenosborne@hotmail.co.uk>
2020-09-23 21:32:31 +0100

    Changed duration to end in clip

--- commands/clip.js
+++ commands/clip.js
@@ -8,12 +8,12 @@ module.exports = {
     args: true,
     usage: '<url> <start> <duration> <name>',
 	execute(message, args) {
-        let [url, start, duration, ...nameArray] = args.map(element => element.replace(/"/g, ''));
+        let [url, start, end, ...nameArray] = args.map(element => element.replace(/"/g, ''));
         let nameString = 'sounds/' + nameArray.join("_")
 
         ytdl.getInfo(url, {quality: 'highestaudio'}, (err, info) => {
             if (err) throw err;
-            const ffmpeg = spawn('ffmpeg', ['-y', '-ss', start, '-t', duration, '-i', info.formats[0].url, '-b:a', '192k', `${nameString}.mp3`]);
+            const ffmpeg = spawn('ffmpeg', ['-y', '-ss', start, '-to', end, '-i', info.formats[0].url, '-b:a', '192k', `${nameString}.mp3`]);
             ffmpeg.stdout.on('data', (data) => {
                 console.log(`stdout: ${data}`);
             });
