31baa46605ac5fcdd9cb62c388bacb9d68ffe964
steveno <stevenosborne@hotmail.co.uk>
2020-11-25 21:50:54 +0000

    Clip fixes

--- commands/clip.js
+++ commands/clip.js
@@ -10,12 +10,13 @@ module.exports = {
 	execute(message, args) {
         let [url, start, duration, ...nameArray] = args.map(element => element.replace(/"/g, ''));
         let nameString = 'sounds/' + nameArray.join("_")
-	console.log('Clipping: ' + nameString);
-	ytdl.getInfo(url, {quality: 'highestaudio'}).then(info => {
-		const ffmpeg = spawn('ffmpeg', ['-y', '-ss', start, '-t', duration, '-i', info.formats[0].url, '-b:a', '192k', `${nameString}.mp3`]);
-		ffmpeg.stdout.on('data', (data) => {
-			console.log(`stdout: ${data}`);
-		});
-	}).catch(error => console.log(error));
+		console.log('Clipping: ' + nameString);
+		ytdl.getInfo(url).then(info => {
+			let format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
+			const ffmpeg = spawn('ffmpeg', ['-y', '-ss', start, '-t', duration, '-i', format.url, '-b:a', '192k', `${nameString}.mp3`]);
+			ffmpeg.stdout.on('data', (data) => {
+				console.log(`stdout: ${data}`);
+			});
+		}).catch(error => console.log(error));
 	},
 };
