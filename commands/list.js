62f2da360a65d986e1b8da86953563a296ab5e12
Steven Osborne <stevenosborne@hotmail.co.uk>
2021-02-13 16:17:53 +0000

    list fix

--- commands/list.js
+++ commands/list.js
@@ -1,25 +1,27 @@
 const fs = require('fs');
 
+splitToChunks(array, chunkSize) {
+    var result = [];
+    for (var i=0,len=array.length; i<len; i+=chunkSize)
+      result.push(array.slice(i,i+chunkSize));
+    return result;
+},
+execute(message, args) {
+    let files = fs.readdirSync('./sounds', {withFileTypes: true})
+        .filter(item => item.name.includes('.mp3'))
+        .filter(item => !item.isDirectory())
+        .map(item => item.name.replace(/_/g, ' ').replace('.mp3', '') + '\n');
+
+    files.sort();
+
+    for (let array of splitToChunks(files, 100)) {
+        message.channel.send("```\n" + array.toString().replace(/,/g, '') + "\n```");
+    }
+},
+
 module.exports = {
 	name: 'list',
     description: 'list all clips',
     args: false,
-    splitToChunks(array, chunkSize) {
-        var result = [];
-        for (var i=0,len=array.length; i<len; i+=chunkSize)
-          result.push(array.slice(i,i+chunkSize));
-        return result;
-    },
-	execute(message, args) {
-        let files = fs.readdirSync('./sounds', {withFileTypes: true})
-            .filter(item => item.name.includes('.mp3'))
-            .filter(item => !item.isDirectory())
-            .map(item => item.name.replace(/_/g, ' ').replace('.mp3', '') + '\n');
-
-        files.sort();
-
-        for (let array of splitToChunks(files, 100)) {
-            message.channel.send("```\n" + array.toString().replace(/,/g, '') + "\n```");
-        }
-	},
+    execute: execute(message, args),
 };
