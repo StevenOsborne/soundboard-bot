8b6d49a7d3473b2a08cd896ca78fcbba2822ba20
Steven Osborne <stevenosborne@hotmail.co.uk>
2021-02-13 16:21:19 +0000

    list fix

--- commands/list.js
+++ commands/list.js
@@ -1,27 +1,26 @@
 const fs = require('fs');
 
-splitToChunks(array, chunkSize) {
+function splitToChunks(array, chunkSize) {
     var result = [];
     for (var i=0,len=array.length; i<len; i+=chunkSize)
       result.push(array.slice(i,i+chunkSize));
     return result;
-},
-execute(message, args) {
-    let files = fs.readdirSync('./sounds', {withFileTypes: true})
-        .filter(item => item.name.includes('.mp3'))
-        .filter(item => !item.isDirectory())
-        .map(item => item.name.replace(/_/g, ' ').replace('.mp3', '') + '\n');
-
-    files.sort();
-
-    for (let array of splitToChunks(files, 100)) {
-        message.channel.send("```\n" + array.toString().replace(/,/g, '') + "\n```");
-    }
 },
 
 module.exports = {
 	name: 'list',
     description: 'list all clips',
     args: false,
-    execute: execute(message, args),
+	execute(message, args) {
+        let files = fs.readdirSync('./sounds', {withFileTypes: true})
+            .filter(item => item.name.includes('.mp3'))
+            .filter(item => !item.isDirectory())
+            .map(item => item.name.replace(/_/g, ' ').replace('.mp3', '') + '\n');
+
+        files.sort();
+
+        for (let array of splitToChunks(files, 100)) {
+            message.channel.send("```\n" + array.toString().replace(/,/g, '') + "\n```");
+        }
+	},
 };
